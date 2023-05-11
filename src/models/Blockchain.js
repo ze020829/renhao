0e460f41ce3d047dec0b00b7a0050cd80104f616
FBCD <745098947@qq.com>
2023-05-11 19:54:49 +0800

    fbcdCoding
    董青区块链构造源代码

--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,22 +1,45 @@
+import { insert } from "ramda";
+
 // Blockchain
 class Blockchain {
+  name;
+  blocks=new Map();
+  genesis;
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含 
       - 名字
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
+  constructor(name,genesis){
+    this.name=name;
+    this.genesis=genesis;
+  }
+  insertHashMap(keys,blocks){
+    this.blocks.set(keys,blocks)
+  }
 
   // 2. 定义 longestChain 函数
   /* 
     返回当前链中最长的区块信息列表
   */
-  longestChain() {
-
-
-    return []
-  }
+    longestChain(blocks) {
+      // let length=blockchain.blocks.height
+      //console.info(blockchain.blocks.get(newBlock.hash).height)
+      var heightArray=[];
+      //整体进行链的长度操作
+    for(var blockss of blocks.values()) {
+        heightArray.push(blockss.height)
+    }
+      return heightArray[heightArray.length-1]
+    }
+    longestChainHash(blocks){
+      var hashArray=[];
+      for(var blockss of blocks.keys()) {
+        hashArray.push(blockss)
+      }
+      return hashArray[hashArray.length-2]
+    }
 }
 
 export default Blockchain
--- src/tests/lesson1.js
+++ src/tests/lesson1.js
@@ -1,48 +1,46 @@
 import Block from '../models/Block.js'
 import Blockchain from '../models/Blockchain.js'
 import sha256 from 'crypto-js/sha256.js'
-
 const main = () => {
   // 初始化区块链
-  var blockchain = new Blockchain('BitCoin')
+  var blockchain = new Blockchain('BitCoin') 
 
   // 创建创世区块
   var genesisBlock = new Block(blockchain, 'root', 0, 'root')
 
   // 设置创世区块
   blockchain.genesis = genesisBlock
-
+  blockchain.insertHashMap(0,blockchain.genesis)
   // 构建区块
   var newBlock = new Block(
     blockchain,
     genesisBlock.hash,
     1,
-    sha256(new Date().getTime().toString()).toString(),
+    sha256(new Date().getTime().toString()).toString()
   )
 
-  blockchain.blocks[newBlock.hash] = newBlock
 
+  blockchain.insertHashMap(newBlock.hash,newBlock)
   var nextBlock = new Block(
     blockchain,
     newBlock.hash,
     2,
-    sha256(new Date().getTime().toString()).toString(),
+    sha256(new Date().getTime().toString()).toString()
   )
 
   var nextCompetitionBlock = new Block(
     blockchain,
     newBlock.hash,
     2,
-    sha256((new Date().getTime() + 1).toString()).toString(),
+    sha256((new Date().getTime() + 1).toString()).toString()
   )
 
   // 添加两个区块高度为 2  的的竞争区块
-  blockchain.blocks[nextBlock.hash] = nextBlock
-  blockchain.blocks[nextCompetitionBlock.hash] = nextCompetitionBlock
+  blockchain.insertHashMap(nextBlock.hash,nextBlock)
+  blockchain.insertHashMap(nextCompetitionBlock.hash,nextCompetitionBlock)
 
-  let longestChain = blockchain.longestChain()
 
-  console.assert(longestChain.length == 2, 'Block height should be 2')
+  console.assert(blockchain.longestChain(blockchain.blocks)==2, 'Block height should be 2')
 
   var thirdBlock = new Block(
     blockchain,
@@ -50,15 +48,13 @@ const main = () => {
     3,
     sha256(new Date().getTime().toString()).toString(),
   )
-
-  blockchain.blocks[thirdBlock.hash] = thirdBlock
-
-  longestChain = blockchain.longestChain()
+  blockchain.insertHashMap(thirdBlock.hash,thirdBlock)
 
   // 区块检查
-  console.assert(longestChain.length == 3, 'Block height should be 2')
+  console.assert(blockchain.longestChain(blockchain.blocks) == 3, 'Block height should be 2')
+  console.info(blockchain.blocks)
   console.assert(
-    longestChain[2].hash == thirdBlock.hash,
+    blockchain.longestChainHash(blockchain.blocks) == thirdBlock.hash,
     `Height block hash should be ${thirdBlock.hash}`,
   )
 }
