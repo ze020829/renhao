a8d79d1f4d72855a7411e4bbb654419c0d0f0275
ze020829 <132757037+ze020829@users.noreply.github.com>
2023-06-07 11:02:42 +0800

    Update Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,15 +1,40 @@
-import sha256 from 'crypto-js/sha256.js'
+const SHA256=require('crypto-js/sha256')
 
-export const DIFFICULTY = 2
 
-class Block {
-  // 1. 完成构造函数及其参数
+class Block{
+    //这里previousHash后面的写法代表可以在实例化时不写参数
+    constructor(timestamp,transactions,previousHash=''){
+this.timestamp=timestamp
+this.transactions=transactions
+this.previousHash=previousHash
+this.nonce=0
+this.hash=this.calclateHash()
+this.isInChain=false
+    }
 
-  constructor() {}
+    calclateHash(){
+       return SHA256(
+            this.index+this.timestamp+JSON.stringify(this.transactions)+this.previousHash+this.nonce
+        ).toString()
+    }
 
-  isValid() {}
+//挖矿
+mineBlock(difficutly){
 
-  setNonce(nonce) {}
+    while( this.calclateHash().substring(0,difficutly)!=Array(difficutly+1).join('0') ){
+    this.nonce++;
+    this.hash=this.calclateHash();
+    }
+    console.log("挖出来的hash为："+this.hash);
+
+    }
+
+hasValidTransaction(){
+    for (const tx of this.transactions) {
+        if(!tx.isValid) return false
+    }
+    return true
 }
 
+}
 export default Block
