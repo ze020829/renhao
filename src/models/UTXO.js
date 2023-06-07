55fc932833238b01f4bac3444ac7b88ea3f96420
ze020829 <132757037+ze020829@users.noreply.github.com>
2023-06-07 11:01:47 +0800

    Update UTXO.js

--- src/models/UTXO.js
+++ src/models/UTXO.js
@@ -1,3 +1,33 @@
-export default class UTXO {
-  constructor() {}
+class UTXO{
+    constructor(fromAddress,toAdress,amount){
+        this.fromAddress=fromAddress
+        this.toAdress=toAdress
+        this.amount=amount
+    }
+
+    //获得交易hash
+calclateHash(){
+    return SHA256(this.fromAddress+this.toAdress+this.amount).toString()
+}
+
+//数字签名
+sigTransaction(singningKey){
+    //交易hash
+const TXhash=this.calclateHash()
+    //hash用私钥签名
+const sig = singningKey.sign(TXhash,'base64')
+    //
+this.signature=sig.toDER('hex')
+
+}
+
+//看是否有篡改交易信息
+isValid(){
+//若fromaddress为空，也要返回有效
+if(this.fromAddress==null) return
+    const publicKey=ec.keyFromPublic(this.fromAddress,'hex')
+    return publicKey.verify(this.calclateHash(),this.signature)
+}
+
+
 }
