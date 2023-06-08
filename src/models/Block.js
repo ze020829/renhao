3a5b8a22a0c390d10c09529454ae7998c4c12e59
ze020829 <132757037+ze020829@users.noreply.github.com>
2023-06-08 22:27:29 +0800

    Update Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,15 +1,29 @@
 import sha256 from 'crypto-js/sha256.js'
 
-export const DIFFICULTY = 2
+export const DIFFICULTY = 3
 
 class Block {
-  // 1. 完成构造函数及其参数
 
-  constructor() {}
+  constructor(_blockchain, _previousHash, _height, _hash, _coinbaseBeneficiary) {
+    this.blockchain = _blockchain
+    this.previousHash = _previousHash
+    this.height = _height
+    this.hash = _hash
+    this.nonce = 0
+    this.coinbaseBeneficiary = _coinbaseBeneficiary
+    this.utxoPool = null
+  }
 
-  isValid() {}
+  isValid() {
+    const prefix = "0".repeat(DIFFICULTY);
+    return this.hash.substring(0, DIFFICULTY) === prefix
+  }
 
-  setNonce(nonce) {}
+  setNonce(_time) {
+    let temp = parseInt(_time) + this.nonce
+    this.hash = sha256(temp.toString()).toString()
+    this.nonce++
+  }
 }
 
 export default Block
