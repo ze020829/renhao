9cf88579365cd62ff55097260a2678693486aa23
ze020829 <132757037+ze020829@users.noreply.github.com>
2023-06-09 19:07:14 +0800

    Update UTXOPool.js

--- src/models/UTXOPool.js
+++ src/models/UTXOPool.js
@@ -1,21 +1,34 @@
 import UTXO from './UTXO.js'
 
 class UTXOPool {
-  constructor(utxos = {}) {}
+  constructor(utxos = {}) {
+    this.utxos = utxos
+  }
+  addUTXO(_miner,_coinbaseBeneficiary,_input,_output) {
+    var utxo = new UTXO()
+    if(this.utxos[_coinbaseBeneficiary] != null){
+      let lastUTXO = this.clone(_miner)
+      utxo.amount += lastUTXO.amount 
+    }
+    utxo.amount += _coinbaseBeneficiary
+    utxo.amount += _input
+    utxo.amount -= _output
+    this.utxos[_miner] = utxo
+  }
+  clone(_player) {
+    return this.utxos[_player]
+  }
+  handleTransaction(_tx) {
+    if (!this.isValidTransaction(_tx)){
+      return
+    }
+    this.utxos[_tx.from].amount -= _tx.value
+    this.addUTXO(_tx.to,0,_tx.value,0)
+  }
+  isValidTransaction(tx) {
+    return this.utxos[tx.from].amount >= tx.value + tx.fee 
+  }
 
-  addUTXO(publicKey, amount) {}
-
-  clone() {}
-
-  // 处理交易函数
-  handleTransaction() {}
-
-  // 验证交易合法性
-  /**
-   * 验证余额
-   * 返回 bool
-   */
-  isValidTransaction() {}
 }
 
 export default UTXOPool
