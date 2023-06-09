53a7a64a9603b5b6ea9eff2a938a2ba3f4f55c5f
ze020829 <132757037+ze020829@users.noreply.github.com>
2023-06-09 19:03:25 +0800

    Update Blockchain.js

--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,42 +1,59 @@
 import UTXOPool from './UTXOPool.js'
 
-Blockchain
 class Blockchain {
-  // 1. 完成构造函数及其参数
-  /* 构造函数需要包含
-      - 名字
-      - 创世区块
-      - 存储区块的映射
-  */
-  constructor() {}
-
-  // 2. 定义 longestChain 函数
-  /*
-    返回当前链中最长的区块信息列表
-  */
+  constructor(_name) {
+    this.name = _name
+    this.genesis = null
+    this.blocks = {}
+  }
   longestChain() {
-    return []
-  }
-
-  // 判断当前区块链是否包含
+    var longestChain = []
+    var temp = 0 
+    var BlockPointer = null 
+    // 遍历所有区块，找到最高的区块高度
+    for (let hash in this.blocks) {
+      // 我们的集合结构是：    hash:区块
+      if (this.blocks[hash].height >= temp) {
+          temp = this.blocks[hash].height
+      }
+    }
+    for (let hash in this.blocks) {
+      if (this.blocks[hash].height === temp) {
+        BlockPointer = this.blocks[hash]
+        longestChain.push(BlockPointer)
+        break
+      }
+    }
+    while (this.blocks[BlockPointer.previousHash] != null) { 
+      longestChain.push(this.blocks[BlockPointer.previousHash])
+      BlockPointer = this.blocks[BlockPointer.previousHash]
+    }
+    return longestChain.reverse()
+    }
   containsBlock(block) {
-    // 添加判断方法
-    return false
+    return this.blocks[block.hash] != undefined
   }
-
-  // 获得区块高度最高的区块
   maxHeightBlock() {
-    // return Block
+    let keysCount = Object.keys(this.blocks).length;
+    if(keysCount == 0){
+      return 0
+    }
+    let height = Object.keys(this.longestChain()).length;
+    return this.longestChain()[height-1]
   }
-
-  // 添加区块
-  /*
-
-  */
   _addBlock(block) {
     if (!block.isValid()) return
     if (this.containsBlock(block)) return
+    var currentBlock = this.maxHeightBlock()
+    let utxoPool = new UTXOPool()
+    if(currentBlock == 0){
+      utxoPool.addUTXO(block.coinbaseBeneficiary,12.5,0,0)
+    }else{
+      let money = currentBlock.utxoPool.utxos[block.coinbaseBeneficiary].amount
+      utxoPool.addUTXO(block.coinbaseBeneficiary,12.5,money,0)
+    }
+    block.utxoPool = utxoPool
+    this.blocks[block.hash] = block
   }
 }
-
 export default Blockchain
