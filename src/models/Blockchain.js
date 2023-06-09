f171ea1c44ea241e70c5da81e3ba297269a159d9
ze020829 <132757037+ze020829@users.noreply.github.com>
2023-06-09 18:57:36 +0800

    Update Blockchain.js

--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -8,16 +8,13 @@ class Blockchain {
   }
   longestChain() {
     var longestChain = []
-    var temp = 0 //用于记录区块高度
+    var temp = 0 
     var BlockPointer = null 
-    // 遍历所有区块，找到最高的区块高度
     for (let hash in this.blocks) {
-      // 我们的集合结构是：    hash:区块
       if (this.blocks[hash].height >= temp) {
           temp = this.blocks[hash].height
       }
     }
-    // 根据区块高度找到最高的区块
     for (let hash in this.blocks) {
       if (this.blocks[hash].height === temp) {
         BlockPointer = this.blocks[hash]
@@ -25,24 +22,16 @@ class Blockchain {
         break
       }
     }
-    // 根据该区块一直向前遍历，直至创世区块
     while (this.blocks[BlockPointer.previousHash] != null) { 
-      //因为高度为1的区块的previousHash是'root'，它在this.blocks里面找不到相同的值，因此会是null
-      // 因为之前已经将最高的区块放进了longestChain数组
-      // 这里每次遍历，是将BlockPointer前一个区块放进longestChain数组
       longestChain.push(this.blocks[BlockPointer.previousHash])
       BlockPointer = this.blocks[BlockPointer.previousHash]
     }
     return longestChain.reverse()
     }
 
-  // 判断当前区块链是否包含
   containsBlock(block) {
-    // 添加判断方法
     return this.blocks[block.hash] != undefined
   }
-
-  // 获得区块高度最高的区块
   maxHeightBlock() {
     // return Block
     let keysCount = Object.keys(this.blocks).length;
@@ -57,8 +46,6 @@ class Blockchain {
   _addBlock(block) {
     if (!block.isValid()) return
     if (this.containsBlock(block)) return
-
-    // 添加 UTXO 快照与更新的相关逻辑
     var currentBlock = this.maxHeightBlock()
     let utxoPool = new UTXOPool()
     if(currentBlock == 0){
