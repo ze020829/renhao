382ed015fed86cd3befacb5628a3fe111fde4b3e
ze020829 <132757037+ze020829@users.noreply.github.com>
2023-06-09 19:06:28 +0800

    Update Transaction.js

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,8 +1,12 @@
 import sha256 from 'crypto-js/sha256.js'
 
 class Transaction {
-  constructor() {
-
+  constructor(_from,_to,_value,_fee) {
+    this.from = _from
+    this.to = _to
+    this.value = _value
+    this.fee = _fee
+    this.hash = sha256(this.from+this.to+this.value+this.fee).toString()
   }
 
   // 更新交易 hash
@@ -12,7 +16,7 @@ class Transaction {
 
   // 计算交易 hash 的摘要函数
   _calculateHash() {
-
+    return sha256(this.from+this.to+this.value+this.fee).toString() //要将手续费一起hash
   }
 }
 
