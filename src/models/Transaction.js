fff5d5be8c868e7e225f4f58af4ef1dbdf303c7c
ze020829 <132757037+ze020829@users.noreply.github.com>
2023-06-09 19:03:53 +0800

    Update Transaction.js

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,19 +1,17 @@
 import sha256 from 'crypto-js/sha256.js'
 
-
 class Transaction {
-  constructor() {
-    
+  constructor(_from,_to,_value) {
+    this.from = _from
+    this.to = _to
+    this.value = _value
+    this.hash = sha256(this.from,this.to,this.value).toString()
   }
-
-  // 更新交易 hash
   _setHash() {
 
   }
-
-  // 计算交易 hash 的摘要函数
   _calculateHash() {
-   
+    return sha256(this.from,this.to,this.value).toString()
   }
 }
 
