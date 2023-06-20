e741c80201fdeb82c22154763f2800a7c2e8cb1c
ze020829 <132757037+ze020829@users.noreply.github.com>
2023-06-20 20:19:39 +0800

    Update UTXOPool.js

--- src/models/UTXOPool.js
+++ src/models/UTXOPool.js
@@ -1,21 +1,40 @@
 import UTXO from './UTXO.js'
 
 class UTXOPool {
-  constructor(utxos = {}) {}
+  constructor(utxos = {}) {
+    this.utxos = utxos
+  }
 
-  addUTXO(publicKey, amount) {}
+  addUTXO(_miner,_coinbaseBeneficiary,_input,_output) {
+    var utxo = new UTXO()
+    if(this.utxos[_coinbaseBeneficiary] != null){
+      let lastUTXO = this.clone(_miner)
+      utxo.amount += lastUTXO.amount 
+    }
+    utxo.amount += _coinbaseBeneficiary
+    utxo.amount += _input
+    utxo.amount -= _output
+    this.utxos[_miner] = utxo
+  }
 
-  clone() {}
+  // 将当前 UTXO 的副本克隆
+  clone(_player) {
+    return this.utxos[_player]
+  }
 
   // 处理交易函数
-  handleTransaction() {}
+  handleTransaction(_tx) {
+    if (!this.isValidTransaction(_tx)){
+      return
+    }
+    this.utxos[_tx.from].amount -= _tx.value
+    this.addUTXO(_tx.to,0,_tx.value,0)
+  }
 
-  // 验证交易合法性
-  /**
-   * 验证余额
-   * 返回 bool
-   */
-  isValidTransaction() {}
+  isValidTransaction(tx) {
+    return this.utxos[tx.from].amount >= tx.value + tx.fee //余额要大于转账金额加上手续费
+  }
+
 }
 
 export default UTXOPool
